d053730f4f39690aa0947b29ae6c5a843f507797
Xuanwo <github@xuanwo.io>
2021-04-23 11:56:43 +0800

    Add read example
    Signed-off-by: Xuanwo <github@xuanwo.io>

--- README.md
+++ README.md
@@ -5,3 +5,6 @@
 - [Create fs Storager](basic/new_fs.go)
 - [Create s3 Storager](basic/new_s3.go) (Amazon S3)
 - [Create cos Storager](basic/new_cos.go) (Tencent Cloud Object Storage)
+- [Read a file](basic/read.go)
+- [Read a range of a file](basic/read.go)
+- [Read a file with callback](basic/read.go)
---
+++ basic/read.go
@@ -0,0 +1,72 @@
+package basic
+
+import (
+	"bytes"
+	"github.com/aos-dev/go-storage/v3/pairs"
+	"github.com/aos-dev/go-storage/v3/types"
+	"log"
+)
+
+func ReadWhole(store types.Storager, path string) {
+	var buf bytes.Buffer
+
+	// Read needs at least two arguments.
+	//
+	// `path` is the path of object.
+	// If path is relative path, the real path will be `store.WorkDir + path`.
+	// If path is absolute path, the real path will be `path`.
+	//
+	// `w`, the `&buf` here is the writer of this operation.
+	// storage will write all content that read into this writer.
+	// It's caller's duty to make sure the writer has been closed.
+	//
+	// Read will return two values.
+	// `n` is the size of read operation.
+	// `err` is the error during this operation.
+	n, err := store.Read(path, &buf)
+	if err != nil {
+		log.Fatalf("read %v: %v", path, err)
+	}
+
+	log.Printf("read size: %d", n)
+	log.Printf("read content: %s", buf.Bytes())
+}
+
+func ReadRange(store types.Storager, path string, offset, size int64) {
+	var buf bytes.Buffer
+
+	// Offset is the read operation's offset.
+	// Size is the read operation's size.
+	//
+	// In this read operation, we will read content in [offset, offset+size).
+	n, err := store.Read(path, &buf,
+		pairs.WithOffset(offset),
+		pairs.WithSize(size),
+	)
+	if err != nil {
+		log.Fatalf("read %v: %v", path, err)
+	}
+
+	log.Printf("read size: %d", n)
+	log.Printf("read content: %s", buf.Bytes())
+}
+
+func ReadWithCallback(store types.Storager, path string) {
+	var buf bytes.Buffer
+
+	cur := int64(0)
+	fn := func(bs []byte) {
+		cur += int64(len(bs))
+		log.Printf("read %d bytes already", cur)
+	}
+
+	// If IoCallback is specified, the storage will call it in every I/O operation.
+	// User could use this feature to implement progress bar.
+	n, err := store.Read(path, &buf, pairs.WithIoCallback(fn))
+	if err != nil {
+		log.Fatalf("read %v: %v", path, err)
+	}
+
+	log.Printf("read size: %d", n)
+	log.Printf("read content: %s", buf.Bytes())
+}
